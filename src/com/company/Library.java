8401ce2d9f347b02b1cd6a5b31cfd1fba313c0c0
Kamyah Rasheed <kamyah.rasheed@stu.jefferson.kyschools.us>
2018-06-12 15:19:33 -0400

    Completed (?) Video Game Library

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -1,4 +1,124 @@
 package com.company;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Scanner;
+
 public class Library {
+
+    private Scanner input = new Scanner (System.in);
+    private List<Game> gamesLibrary = new ArrayList<Game>();
+    private List<Game> checkedOutGames = new ArrayList<Game>();
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yy HH:mm:ss");
+    private Menu menu;
+
+
+    public Library(Menu menu) {
+        this.menu = menu;
+    }
+
+    public void removeGame(int index){
+        if (gamesLibrary.isEmpty()){
+            System.out.println("There are no games in your library, add a game to be able to remove.");
+        } else {
+        gamesLibrary.remove(index);
+        System.out.println("This game has been removed fro your library."); }
+
+        menu.startMenu();
+    }
+
+    public void addGame(){
+        System.out.println("What is the title of your game?");
+        String title = input.nextLine();
+        System.out.println("What type of game is this?");
+        String type = input.nextLine();
+        System.out.println("What is the game's ID Number?");
+        int idNumber = input.nextInt();
+        input.nextLine();
+        Game game = new Game(title, type, idNumber);
+        gamesLibrary.add(game);
+        System.out.println("You have added " +game.getTitle() + " to your library, this game can now be checked out. \n");
+        menu.startMenu();
+    }
+
+    public void listGamesInLibrary(String location) {
+        if (gamesLibrary.isEmpty()){
+            System.out.println("There are no games in your library.");
+        } else {
+
+            int index = 1;
+            for (Game inLibrary : gamesLibrary) {
+
+                System.out.println(index++ + ": " + inLibrary.getTitle());
+            }
+        }
+        if (location.equals("inLibrary")) {
+            menu.startMenu();
+        }
+
+
+    }
+    public void checkOutGame(int index){
+
+        if(gamesLibrary.isEmpty()){
+            System.out.println("There are no games in your library, add some games to be able to check out.");
+        } else {
+            Game game = gamesLibrary.get(index);
+
+            //Create an instance of the calendar object
+            Calendar calendar = Calendar.getInstance();
+            //Ad 7 dayd to the current date
+            String dueDate = dateFormat.format(calendar.getTime());
+            //Uses the line above to set a due date in the future
+            calendar.add(Calendar.DAY_OF_YEAR, 7);
+            //Tell user what their due date is
+            System.out.println(game.getTitle() + " is due on " + dueDate);
+            //set dueDate for this game
+            game.setDueDate(dueDate);
+            //Add game to checked out list
+            checkedOutGames.add(game);
+            //Remove game from Library
+            gamesLibrary.remove(game);
+        }
+        menu.startMenu();
+    }
+    public void checkInGame (int index) {
+
+        Game game = checkedOutGames.get(index);
+        gamesLibrary.add(game);
+
+        try {
+            Calendar calendar = Calendar.getInstance();
+            if (dateFormat.parse(dateFormat.format(calendar.getTime())).before(dateFormat.parse(game.getDueDate()))) {
+                System.out.println("Thanks for turning your game in on time!");
+            } else {
+                System.out.println("Shame on you! You were late turning in a game! :(");
+            }
+        } catch(ParseException pe){
+            //we will leave this empty since we don'1t really need to do anything if we catch this exception
+        }
+        checkedOutGames.remove(game);
+        menu.startMenu();
+        //get game using index from checked out games
+
+    }
+
+    public void listCheckedOut(String location){
+
+        if (checkedOutGames.isEmpty()){
+            System.out.println("There are no games currently checked out.");
+            menu.startMenu();
+        } else {
+            int index = 0;
+            for (Game game: checkedOutGames){
+                System.out.println(++index + ": " +game.getTitle());
+            }
+        }
+        if (location.equals("viewCheckedOut")){
+            menu.startMenu();
+        }
+    }
 }
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Menu {
 
     private Scanner input = new Scanner (System.in);
+    private Library library = new Library(this);
 
     //menu to prompt uer for Library options
     public void startMenu(){
@@ -17,27 +18,47 @@ public class Menu {
                     "\n2. Remove a game from the library" +
                     "\n3. View what is currently in the library" +
                     "\n4. Check out a game" +
-                    "\n5/ Check in a game" +
+                    "\n5. Check in a game" +
                     "\n6. View checked out games" +
                     "\n7. Exit the program");
 
             switch (input.nextInt()) {
                 case 1:
+                    input.nextLine();
                     //Add a game
+                    System.out.println("You have chosen to add a game.");
+                    library.addGame();
                     break;
                 case 2:
+                    System.out.println("Which game would you like to remove?");
+                    library.listGamesInLibrary("forRemoval");
+                    library.removeGame(input.nextInt() - 1);
                     //Remove a game
                     break;
                 case 3:
+                    input.nextLine();
                     //View games in library
+                    library.listGamesInLibrary("inLibrary");
+                    System.out.println("\n");
                     break;
                 case 4:
+                    input.nextLine();
                     //Check out a game
+                    System.out.println("You have chosen to check out a game" +
+                            "\nHere is a list of all games available to check out:");
+                    library.listGamesInLibrary("checkOut");
+                    System.out.println("Choose a number for the game you would like:");
+                    library.checkOutGame(input.nextInt() - 1);
                     break;
                 case 5:
+                    input.nextLine();
                     //Check in a game
+                    System.out.println("WHat game are you checking in?");
+                    library.listCheckedOut("CheckIn");
+                    library.checkInGame(input.nextInt() -1);
                     break;
                 case 6:
+                    library.listCheckedOut("viewCheckedOut");
                     //View checked out games
                     break;
                 case 7:
@@ -47,7 +68,7 @@ public class Menu {
                     break;
                 default:
                     //Exit program
-                    System.out.println("That's not a valid entry. Please pick a number between 1-7/");
+                    System.out.println("That's not a valid entry. Please pick a number between 1-7.");
                     startMenu();
                     break;
 
@@ -56,6 +77,11 @@ public class Menu {
             System.out.println(ime.toString());
             input.nextLine();
             System.out.println("That's not a valid entry. Please pick a number between 1-7.");
+            startMenu();
+        } catch (IndexOutOfBoundsException ioobe){
+
+            input.nextLine();
+            System.out.println("You have not entered a correct number. Taking you back to the main menu!");
             startMenu();
         }
     }
